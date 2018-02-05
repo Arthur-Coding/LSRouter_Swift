5b2a322432a521bf62f0ea3eda2da9ccb37f8d5e
ArthurShuai <zhixingui_liushuai@163com>
2018-02-05 21:06:36 +0800

    update

--- LSRouter_Swift.swift
+++ LSRouter_Swift.swift
@@ -17,6 +17,8 @@ public typealias LSInformationHandler = (Any)->Void
 ///   - url: 格式：scheme://[target]/[actionName]?[params]
 ///          例如：aaa://targetA/actionB?id=1234&key=4567
 ///   - completion: 完成回调
+@discardableResult
+@available(iOS 8.0, macOS 10.11, tvOS 9.0, *)
 public func performAction(url:URL, completion:LSRouterHandler?)
 {
     LSRouter.performAction(url: url, completion: completion)
@@ -29,6 +31,8 @@ public func performAction(url:URL, completion:LSRouterHandler?)
 ///   - actionName: 待执行方法名，组件的方法名前必须添加前缀@objc
 ///   - params: 待执行方法的参数
 ///   - perform: 找到组件后下一步调用处理，如push、present组件等
+@discardableResult
+@available(iOS 8.0, macOS 10.11, tvOS 9.0, *)
 public func openModule(objectClass:String, actionName:String?, params:Any?, perform:LSRouterHandler?)
 {
     LSRouter.openModule(objectClass: objectClass, actionName: actionName, params: params, perform: perform)
@@ -38,6 +42,8 @@ public func openModule(objectClass:String, actionName:String?, params:Any?, perf
 ///
 /// - Parameter objectClass: 组件类名
 /// - Returns: YES or NO
+@discardableResult
+@available(iOS 8.0, macOS 10.11, tvOS 9.0, *)
 public func releaseModule(objectClass:String) -> Bool
 {
     return LSRouter.releaseModule(objectClass: objectClass)
@@ -49,6 +55,8 @@ public func releaseModule(objectClass:String) -> Bool
 /// - Parameters:
 ///   - information: 通讯信息
 ///   - tagName: 通讯标记，用于接收方识别接收信息
+@discardableResult
+@available(iOS 8.0, macOS 10.11, tvOS 9.0, *)
 public func sendInformation(information:Any, tagName:String)
 {
     LSRouter.sendInformation(information: information, tagName: tagName)
@@ -60,6 +68,8 @@ public func sendInformation(information:Any, tagName:String)
 /// - Parameters:
 ///   - tagName: 通讯标记，用于接收方识别接收信息
 ///   - result: 接收到通讯信息回调，将返回通讯信息
+@discardableResult
+@available(iOS 8.0, macOS 10.11, tvOS 9.0, *)
 public func receiveInformation(tagName:String, result:@escaping LSInformationHandler)
 {
     LSRouter.receiveInformation(tagName: tagName, result: result)
